b67d68d53ae6c8b67c2a3072552c9d3a830957bf
Vishnu V <vishnuvoffcl@gmail.com>
2024-05-12 19:54:44 +0530

    added posting threads and comments features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const result = await fetchPosts(1, 30);
   const user = await currentUser();
 
-  console.log(result)
+  // console.log(result)
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -5,7 +5,6 @@ export const ThreadValidation = z.object({
     accountId: z.string(),
 })
 
-export const CoomentValidation = z.object({
-    thread: z.string().min(3, {message: "minimum 3 characters"}),
-    accountId: z.string(),
-})
+export const CommentValidation = z.object({
+    thread: z.string().min(3, { message: "Minimum 3 characters." }),
+  });
