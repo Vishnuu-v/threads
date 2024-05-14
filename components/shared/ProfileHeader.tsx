544b37ab381987fc5af63f9cffe07ab364070be6
Vishnu V <vishnuvoffcl@gmail.com>
2024-05-14 20:28:43 +0530

    added community features

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -15,7 +15,12 @@ const ProfileHeader = ({accountId, authUser, name, username, imgUrl,bio}: Props)
         <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
                 <div className="relative h-20 w-20 object-cover">
-                    <Image src={imgUrl} alt="Profile image" fill className="rounded-full object-contain shadow-2xl" />
+                    <Image 
+                        src={imgUrl} 
+                        alt="Profile image" 
+                        fill 
+                        className="rounded-full object-contain shadow-2xl" 
+                    />
                 </div>
                 <div className="flex-1">
                     <h2 className="text-left text-heading3-bold text-light-1">{name}</h2>
